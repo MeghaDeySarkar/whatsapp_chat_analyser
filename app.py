02dc8773d2383397ecc0173dceac36d158f0e36e
Megha Dey Sarkar <moonsarkar204@gmail.com>
2024-04-20 16:19:38 +0800

    uncomment changes

--- app.py
+++ app.py
@@ -128,8 +128,8 @@ if uploaded_file is not None:
         with col1:
             st.dataframe(emoji_df)
             
-        # with col2:
-        #     fig,ax = plt.subplots()
-        #     ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
-        #     st.pyplot(fig)
+        with col2:
+            fig,ax = plt.subplots()
+            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
+            st.pyplot(fig)
         
